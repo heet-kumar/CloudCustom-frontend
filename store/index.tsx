98a9dde5bf994d3e26189edfda36fe1ef2c27500
Heet kumar Kothadiya <heetkumarpatel@gmail.com>
2023-10-31 17:48:13 +0530

    store updated with SubService slice

--- store/index.tsx
+++ store/index.tsx
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import serviceReducer from "./slice/serviceSlice";
+import subServiceReducer from "./slice/subServiceSlice";
 
 const store = configureStore({
     reducer:{
         service: serviceReducer,
+        subService: subServiceReducer
     }
 });
 
