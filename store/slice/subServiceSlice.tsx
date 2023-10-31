1c73e8015a943396980788ea0de577ed88e837cc
Heet kumar Kothadiya <heetkumarpatel@gmail.com>
2023-10-31 17:47:29 +0530

    SubService Slice implemented

--- store/slice/subServiceSlice.tsx
+++ store/slice/subServiceSlice.tsx
@@ -5,28 +5,26 @@ const subServiceSlice = createSlice({
     initialState: {
         subService: [
             {
-                {
-                    id: 100,
-                    sid: 30,
-                    name: "Virtual Machine",
-                    dsc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                    columns : '["Name","Region","Machine Family","CPUs","Memory","Boot Disk Size","OS","Allow traffic"]'
-                },
-                {
-                    id: 101,
-                    sid: 30,
-                    name: "Kubernate Engine",
-                    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-                    columns : '["Name","Region","Machine Family","CPUs","Memory","Boot Disk Size","OS","Allow traffic"]'
-                },
-                {
-                    id: 103,
-                    sid: 30,
-                    name: "Kubernate Engine",
-                    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-                    columns : '["Name"]'
-                },
-            }
+                id: 100,
+                sid: 30,
+                name: "Virtual Machine",
+                dsc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                columns : '["Name","Region","Machine Family","CPUs","Memory","Boot Disk Size","OS","Allow traffic"]'
+            },
+            {
+                id: 101,
+                sid: 30,
+                name: "Kubernate Engine",
+                desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                columns : '["Name","Region","Machine Family","CPUs","Memory","Boot Disk Size","OS","Allow traffic"]'
+            },
+            {
+                id: 103,
+                sid: 30,
+                name: "Kubernate Engine",
+                desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                columns : '["Name"]'
+            },
         ],
     },
     reducers: {
